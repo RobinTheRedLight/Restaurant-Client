3a7c98c81971e144f80bc9290724da14a7ac8ebc
RobinTheRedLight <robinmitra789@gmail.com>
2023-12-08 11:26:31 +0600

    useMenu Hook Added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react";
 import Menu from "../../../Components/Menu";
 import SectionTitle from "../../../Components/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((i) => i.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
   return (
     <section>
       <SectionTitle subHeading="Check it out" heading="FROM OUR MENU" />
 
       <div className="grid md:grid-cols-2 gap-10 px-10 pb-20">
-        {menu.map((i) => (
+        {popular.map((i) => (
           <Menu key={i._id} menu={i} />
         ))}
       </div>
