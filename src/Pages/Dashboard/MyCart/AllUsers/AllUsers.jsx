6703498193f40864caa48fabd581bcef1632776b
RobinTheRedLight <robinmitra789@gmail.com>
2024-01-23 17:13:33 +0600

    User data loaded on admin panel

--- src/Pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/MyCart/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery({
@@ -10,7 +11,25 @@ const AllUsers = () => {
       return res.json();
     },
   });
-const handleMakeAdmin = id => {}
+  const handleMakeAdmin = (user) => {
+    fetch(`http://localhost:5000/users/admin/${user._id}`, {
+      method: "PATCH",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an Admin Now!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
   const handleDelete = (user) => {};
 
   return (
@@ -53,7 +72,7 @@ const handleMakeAdmin = id => {}
                     "admin"
                   ) : (
                     <button
-                      onClick={() => handleMakeAdmin(user._id)}
+                      onClick={() => handleMakeAdmin(user)}
                       className="text-white btn bg-orange-600"
                     >
                       <FaUserShield />
